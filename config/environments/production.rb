1a4364d1515f137972a8e533e3729e9d436cfb01
lavish <lavishkushwah30@gmail.com>
2020-06-22 09:28:45 +0530

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "https://wishingbirth.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :authentication => :plain,
+   :domain => 'gmail.com',
+   :user_name => 'lavy301195@gmail.com',
+   :password => 'lavykush3011',
+   }
 end
